5f86743448abc20b8a89a483f58c1c1c7344aef5
Vsabate <victorsabat8@gmail.com>
2023-01-21 10:53:33 +0100

    Score Manager script created

---
+++ SabatéVíctor_Tetris/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    #region VARIABLES
+    //[HideInInspector]
+    public int s_currentScore, s_maxScore;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
